f9bb274054750dd8b0fe9ac38180b0805df60fbb
Arzumanyasha <arzumanianartur0@gmail.com>
2019-07-27 22:32:12 +0300

    Add field injection

--- app/src/main/java/com/arturarzumanyan/daggerexample/MainActivity.kt
+++ app/src/main/java/com/arturarzumanyan/daggerexample/MainActivity.kt
@@ -2,17 +2,18 @@ package com.arturarzumanyan.daggerexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val car: Car
-
         val carComponent: CarComponent = DaggerCarComponent.create()
-        car = carComponent.getCar()
+        carComponent.inject(this)
+
         car.drive()
     }
 }
